422bc0f6084f3b383d43a47dc4377a1dce0fe4c7
Fernando M Tenório <fernando.tenorio@tecnospeed.com.br>
2018-10-28 10:14:46 -0300

    fix: example

--- examples/simple.js
+++ examples/simple.js
@@ -1,5 +1,5 @@
 const freeloader = require('../')
-const url = 'https://github.com/dotenorio/freeloader/raw/master/site/index.html'
+const url = 'https://raw.githubusercontent.com/dotenorio/freeloader/master/examples/site/index.html'
 
 freeloader(url, (err, meta) => {
   if (err) throw err
